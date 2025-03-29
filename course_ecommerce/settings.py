56ff9e6feb1dea1ddad74a9f5e44d285a7edd182
Maulidi Mdami <maulidimdami@gmail.com>
2025-03-29 16:52:28 +0300

    Refined settings

--- course_ecommerce/settings.py
+++ course_ecommerce/settings.py
@@ -78,12 +78,27 @@ WSGI_APPLICATION = 'course_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'useuulkn_main_database',  # Change this to your cPanel database name
+        'USER': 'useuulkn_admin_user',    # Change this to your cPanel database username
+        'PASSWORD': 'p};Ju^%AY&]h',  # Change this to your cPanel database password
+        'HOST': 'localhost',  # Change this if using a remote MySQL host
+        'PORT': '3306',       # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 
 
 # Password validation
