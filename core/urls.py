e355401f503f5d47648bb2f2bac5f660d09136de
Maulidi Mdami <maulidimdami@gmail.com>
2025-03-29 17:47:27 +0300

    aDDED urls

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 from django.contrib.auth import views as auth_views  # Import auth views
 from . import views
 
+from django.http import HttpResponse
+import os
+
+def acme_challenge(request, challenge):
+    challenge_path = os.path.join('/home/useuulkn/repositories/course_ecommerce/.well-known/acme-challenge/', challenge)
+    if os.path.exists(challenge_path):
+        with open(challenge_path, 'r') as f:
+            return HttpResponse(f.read(), content_type="text/plain")
+    return HttpResponse("Not Found", status=404)
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('courses/', views.CourseListView.as_view(), name='course_list'),
@@ -13,4 +24,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),  # Use your custom logout view
+
+    path(".well-known/acme-challenge/<str:challenge>", acme_challenge),
 ]
